2950acadb9fb73ab68d255acddfa8383c410edc0
shubham135sd <39730257+shubham135sd@users.noreply.github.com>
2022-01-11 20:15:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 # Constants
 mu = 1.81*pow(10,-5)    # dynamic viscosity
 rho = 1.225
 nu = mu/rho
 
-# Variables
-omega = 20-20000        # frequency (to be inserted as an nparray?) 
 Vinf = 20
 
 # XFOIL ouput
@@ -19,7 +18,13 @@ Tw = 0.5*rho*Vinf*Vinf * Cf     # Shear stress
 
 # Dependent variables
 Rt = 0.11* pow(Ue*theta/nu,0.75)
-k = omega*delta/Ue
 
-# Goody's equation for surface pressure spectrum
-phi = (Tw*Tw*delta/Ue) * (3*k*k) / ( pow(pow(k,0.75) + 0.5, 3.7) + pow(1.1*pow(Rt,-0.57)*k, 7) )
+phi_pp = np.zeros(9901)
+w = 0
+for omega in range (100,10001):
+    k = omega*delta/Ue
+    # Goody's equation for surface pressure spectrum
+    phi_pp[w] = (Tw*Tw*delta/Ue) * (3*k*k) / ( pow(pow(k,0.75) + 0.5, 3.7) + pow(1.1*pow(Rt,-0.57)*k, 7) )
+    w+=1
+
+print(phi_pp)
